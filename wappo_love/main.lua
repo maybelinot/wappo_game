1292b86b3ce7b78bdd20c96ae57ffe0148e61432
maybelinot <edik.trott@yandex.ru>
2015-06-27 23:57:18 +0200

    Initial commit

--- wappo_love/main.lua
+++ wappo_love/main.lua
@@ -9,9 +9,7 @@ function level_load(number)
     level.size.y = tiled_level['height']
     level.map = tiled_level['data']
     level.floor_obj = {}
-    level.floor_map = {}
     level.units_obj = {}
-    level.units_map = {}
     level.player = nil
     level.is_moving = false
     for i=0,level.size.x-1 do
@@ -21,8 +19,6 @@ function level_load(number)
                 local object = get_object[cell]()
                 object.x = i
                 object.y = j
-                level.units_map[i*level.size.x + j] = object
-                level.floor_map[i*level.size.x + j] = nil
                 if cell == 1 then
                     level.player = object
                 else
@@ -32,12 +28,7 @@ function level_load(number)
                 local object = get_object[cell]()
                 object.x = i
                 object.y = j
-                level.units_map[i*level.size.x + j] = nil
-                level.floor_map[i*level.size.x + j] = object
                 table.insert(level.floor_obj, object)
-            else
-                level.units_map[i*level.size.x + j] = nil
-                level.floor_map[i*level.size.x + j] = nil
             end
         end
     end
@@ -51,6 +42,15 @@ end
 
 local sum = 0
 local num = 0
+
+function take_element(list, x, y)
+    for i=1, #list do
+        if list[i].x==x and list[i].y==y then
+            return list[i]
+        end
+    end
+    return nil
+end
 
 function level_draw(level)
     for i=1,#level.floor_obj do
@@ -73,24 +73,20 @@ function level_update(level, dt)
 end
 
 function level_move(level, key, way)
-    local cell = level.floor_map[(level.player.x+way[1])*level.size.x + (level.player.y+way[2])]
+    local cell = take_element(level.floor_obj, level.player.x+way[1], level.player.y+way[2])
     if cell ~= nil then
         print("cant move, obstacle")
     else
-        local cell = level.units_map[(level.player.x+way[1]*2)*level.size.x + (level.player.y+way[2]*2)]
+        local cell = take_element(level.units_obj, level.player.x+way[1]*2, level.player.y+way[2]*2)
         if cell == nil then
-            level.units_map[level.player.x*level.size.x + level.player.y] = nil
-            local object = get_object[1]()
-            object.x = level.player.x + way[1]*2
-            object.y = level.player.y + way[2]*2
-            object.sprite = level.player.animations[key]
-            object.anim_x = way[1]*(-52)
-            object.anim_y = way[2]*(-40)
-            level.units_map[(level.player.x+way[1]*2)*level.size.x + (level.player.y+way[2]*2)] = object
-            level.player = object
+            level.player.x = level.player.x + way[1]*2
+            level.player.y = level.player.y + way[2]*2
+            level.player.sprite = level.player.animations[key]
+            level.player.anim_x = way[1]*(-52)
+            level.player.anim_y = way[2]*(-40)
             level.is_moving = true 
             flux.to(level.player, 1.2, { anim_x = 0, anim_y = 0 }):ease("circinout"):oncomplete(function () level.is_moving = false end)
-            local cell = level.floor_map[(level.player.x)*level.size.x + (level.player.y)]
+            local cell = take_element(level.floor_obj, level.player.x, level.player.y)
             if cell == nil then
                 print("move ok")
             elseif cell.index == 6 then
@@ -100,8 +96,6 @@ function level_move(level, key, way)
                 for i=1,#level.floor_obj do
                     if level.floor_obj[i].index==7 and (level.floor_obj[i].x ~= level.player.x or level.floor_obj[i].y ~= level.player.y) then
                         print('ok')
-                        level.units_map[level.player.x*level.size.x + level.player.y] = nil
-                        level.units_map[level.floor_obj[i].x*level.size.x + level.floor_obj[i].y] = get_object[1]()
                         level.player.x = level.floor_obj[i].x
                         level.player.y = level.floor_obj[i].y
                         break
@@ -109,12 +103,13 @@ function level_move(level, key, way)
                 end
             end
         elseif cell.index == 5 then
-            local cell = level.floor_map[(level.player.x+way[1]*3)*level.size.x + (level.player.y+way[2]*3)]
+            local cell = take_element(level.floor_obj, level.player.x+way[1]*3, level.player.y+way[2]*3)
             if cell ~= nil and cell.index >=13 and cell.index <= 16 then
                 print("cant move flame to obstacle")
             else
-                local pos = (level.player.x+way[1]*4)*level.size.x + (level.player.y+way[2]*4)
-                if level.units_map[pos] == nil and level.floor_map[pos] == nil then
+                local unit = take_element(level.units_obj, level.player.x+way[1]*4, level.player.y+way[2]*4)
+                local floor = take_element(level.floor_obj, level.player.x+way[1]*4, level.player.y+way[2]*4)
+                if unit == nil and floor == nil then
                     for i=1,#level.units_obj do
                         if level.units_obj[i].index == 5 and level.units_obj[i].x == level.player.x+way[1]*2 and level.units_obj[i].y == level.player.y+way[2]*2 then
                             table.remove(level.units_obj, i)
@@ -126,9 +121,6 @@ function level_move(level, key, way)
                     object.x = level.player.x+way[1]*4
                     object.y = level.player.y+way[2]*4
                     table.insert(level.units_obj, object)
-                    level.units_map[level.player.x*level.size.x + level.player.y] = nil
-                    level.units_map[(level.player.x+way[1]*2)*level.size.x + (level.player.y+way[2]*2)] = get_object[1]()
-                    level.units_map[(level.player.x+way[1]*4)*level.size.x + (level.player.y+way[2]*4)] = get_object[5]()
                     level.player.x = level.player.x + way[1]*2
                     level.player.y = level.player.y + way[2]*2
                     level.player.sprite = level.player.animations[key]
