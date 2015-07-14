227f845bfecc6d9194a06d2e4ebcd7645734d4fd
maybelinot <edik.trott@yandex.ru>
2015-07-14 21:15:21 +0200

    Scroll acceleration

--- menu/panel.lua
+++ menu/panel.lua
@@ -15,7 +15,8 @@ function Panel:initialize(positions, delta_buttons, button_positions, sprite, sc
 	self.buttons.delta = delta_buttons
 	self.buttons.positions = button_positions
 	self.scrolled = scrolled
-	self.scroll = 0
+	self.scroll_pos = 0
+	self.scroll_acc = 0
 end
 
 function Panel:add_button(text, callback)
@@ -24,7 +25,7 @@ end
 
 function Panel:draw()
 	for i=1, #self.list do
-		self.list[i]:draw(self.x, self.y + (self.buttons.positions[4] + self.buttons.delta)*(i-1) + self.scroll)
+		self.list[i]:draw(self.x, self.y + (self.buttons.positions[4] + self.buttons.delta)*(i-1) + self.scroll_pos)
 	end
 end
 
@@ -32,6 +33,23 @@ function Panel:update(dt)
 	-- for i=1, #self.list do
 	-- 	self.list[i]:update(dt)
 	-- end
+	if self.scrolled then
+		if self.scroll_pos+ self.scroll_acc*dt < - #self.list* (self.buttons.positions[4] + self.buttons.delta) + self.size.y then
+			self.scroll_pos = - #self.list* (self.buttons.positions[4] + self.buttons.delta) + self.size.y
+		elseif self.scroll_pos+ self.scroll_acc*dt > 0 then
+			self.scroll_pos = 0
+		else
+			self.scroll_pos = self.scroll_pos + self.scroll_acc*dt
+		end
+		print()
+		print(math.log(self.scroll_acc))
+		if self.scroll_acc > 0  then
+			self.scroll_acc = self.scroll_acc - math.log(math.abs(self.scroll_acc)+1)/3
+		else
+			self.scroll_acc = self.scroll_acc + math.log(math.abs(self.scroll_acc)+1)/3
+		end
+		print(self.scroll_acc)
+	end
 end
 
 function Panel:mousepressed(x, y)
@@ -40,7 +58,7 @@ function Panel:mousepressed(x, y)
     y = y - self.y
     if x <= self.size.x and x>0 and 
     		y <= self.size.y and y>0  then
-    	y = y - self.scroll
+    	y = y - self.scroll_pos
     	local i = y/(self.buttons.positions[4] + self.buttons.delta)
     	if (math.ceil(i)-i)*(self.buttons.positions[4] + self.buttons.delta) < self.buttons.positions[4]  and math.ceil(i)<=#self.list then
 			self.list[math.ceil(i)].callback()
@@ -49,14 +67,27 @@ function Panel:mousepressed(x, y)
 end
 
 function Panel:scroll_up()
-	if self.scrolled and self.scroll > - #self.list* (self.buttons.positions[4] + self.buttons.delta) + self.size.y then
-		self.scroll = self.scroll - 10
+	if self.scroll_acc > 0 then
+		self.scroll_acc = 0
+		return
+	end
+	print(self.scroll_pos, - #self.list* (self.buttons.positions[4] + self.buttons.delta) + self.size.y, self.scroll_acc)
+	if self.scrolled and self.scroll_pos > - #self.list* (self.buttons.positions[4] + self.buttons.delta) + self.size.y then
+		self.scroll_acc = self.scroll_acc - 15
+	else
+		self.scroll_acc = 0
 	end
 end
 
 function Panel:scroll_down()
-	if self.scrolled and self.scroll < 0 then
-		self.scroll = self.scroll + 10
+	if self.scroll_acc < 0 then
+		self.scroll_acc = 0
+		return
+	end
+	if self.scrolled and self.scroll_pos <= 0 then
+		self.scroll_acc = self.scroll_acc + 15
+	else
+		self.scroll_acc = 0
 	end
 end
 
