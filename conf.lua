df282165fd70c3b492e0b142f123539a9d28ab7f
Mark Birger <birgerm@yandex.ru>
2020-04-04 12:21:25 +0200

    Bump Love2d version

--- conf.lua
+++ conf.lua
@@ -1,6 +1,6 @@
 function love.conf(t)
     t.identity = nil                   -- The name of the save directory (string)
-    t.version = "0.9.2"                -- The LÖVE version this game was made for (string)
+    t.version = "0.11.3"                -- The LÖVE version this game was made for (string)
     t.console = true                  -- Attach a console (boolean, Windows only)
  
     t.window.title = "Untitled"        -- The window title (string)
